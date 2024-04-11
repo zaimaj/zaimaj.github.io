0f27e4656ee508772553f7e46c3e5c2e78ee32ad
zaimaj <159507813+zaimaj@users.noreply.github.com>
2024-04-11 14:25:51 -0700

    hw8 done

---
+++ homeworks/hw8/assets/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("toggle-button").onclick = toggleSongs;
+
+
+
+function toggleSongs() {
+    document.getElementById("songs-second-half").classList.toggle("hidden");
+}
+
+
+
+
